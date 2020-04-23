8cdacb0b37150e4fb85221e24bf3862f36d9cb06
1456121347 <1456121347@qq.com>
2020-04-23 16:15:07 +0800

    自定义url处理类&&数据库创建

--- web/static/js/common.js
+++ web/static/js/common.js
@@ -1,18 +1,18 @@
-// ;
-// var common_ops = {
-//     init:function(){
-//         this.eventBind()
-//     },
-//     eventBind:function(){
-//         console.log("common.js")
-//     },
-//     buildUrl:function(path,params){
-//         var url = "" + path
+;
+var common_ops = {
+    init:function(){
+        this.eventBind()
+    },
+    eventBind:function(){
+        console.log("common.js")
+    },
+    buildUrl:function(path,params){
+        var url = "" + path
 
-//         return url;
-//     }
-// }
+        return url;
+    }
+}
 
-// $(document).ready(function(){
-//     common_ops.init();
-// })
+$(document).ready(function(){
+    common_ops.init();
+})
--- web/static/js/user/login.js
+++ web/static/js/user/login.js
@@ -5,31 +5,42 @@ var user_login_ops = {
     },
     eventBind:function(){
         $(".login_wrap .do-login").click(function(){
+            var btn_target = $(this)
+            if (btn_target.hasClass("disabled")) {
+                alert("请不要重复提交")
+                return;
+            }
             var login_name = $(".login_wrap input[name=login_name]").val()
             var login_pwd = $(".login_wrap input[name=login_pwd]").val()
             console.log(login_name)
             console.log(login_pwd)
 
-            // 前端校检
+            // 前端校检 不为空 长度不小于1
             if (login_name == undefined || login_name.length < 1){
                 alert('请输入正确的用户名')
-                return
+                return;
             }
             if (login_pwd == undefined || login_pwd.length < 1){
                 alert('请输入正确的密码')
-                return
+                return;
             }
+            btn_target.addClass("disabled")
 
-            // $.ajax({
-            //     url:common_ops.buildUrl("/user/login"),
-            //     type:"POST",
-            //     data:{'login_name':login_name,'login_pwd':login_pwd},
-            //     dataType:'json',
-            //     success:function(resp){
-            //         console.log(resp)
-            //         alert(resp.msg)
-            //     }
-            // })
+            // Ajax 前后端分离的，数据格式json {key:value},前端发送数据是json格式，所以后端返回的也是json格式
+            
+            
+            $.ajax({
+                // url:common_ops.buildUrl("/user/login"),
+                url:"login",
+                type:"POST",
+                data:{'login_name':login_name,'login_pwd':login_pwd},
+                dataType:'json',
+                success:function(resp){
+                    btn_target.removeClass("disabled")
+                    console.log(resp)
+                    alert(resp.msg)
+                }
+            })
         })
     }
 }
