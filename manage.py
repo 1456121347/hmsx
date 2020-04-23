af6cced342e79915d5f1d306e4af6afa5a72f0a9
1456121347 <1456121347@qq.com>
2020-04-23 11:00:57 +0800

    login登录页面

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 from application import app,manager
-
 from flask_script import Server
 import urls
 
-# web 服务器
-manager.add_command("runserver",Server(host="localhost",port=5000,use_debugger=True,use_reloader=True))
+# 配置runserver指令
+manager.add_command("runserver",Server(host="localhost",port="5000",use_debugger=True,use_reloader=True))
 
-def main(): 
+
+def main():
     manager.run()
 
 
-if __name__ =="__main__":
+if __name__ == '__main__':
     try:
         import sys
         sys.exit(main())
