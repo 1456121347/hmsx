9f0b1863a4b7de17250296d0aa6017209de649cf
1456121347 <1456121347@qq.com>
2020-04-29 13:54:33 +0800

    分页

--- web/interceptos/AuthInterceptor.py
+++ web/interceptos/AuthInterceptor.py
@@ -1,68 +1,60 @@
-# -*- coding: utf-8 -*-
+from flask import request,redirect,g
+
+from common.libs.UrlManager import UrlManager
+from common.libs.user.UserService import UserService
+from common.models.User import User
 from application import app
-from flask import request,g,redirect
 
-from common.models.User import ( User )
-from common.libs.user.UserService import ( UserService )
-from common.libs.UrlManager import ( UrlManager )
-import  re
+import re
 
+# 每次请求之前都要先经过这里
 @app.before_request
 def before_request():
-    ignore_urls = app.config['IGNORE_URLS']
-    ignore_check_login_urls = app.config['IGNORE_CHECK_LOGIN_URLS']
-    path = request.path
+   path = request.path
+   ignore_urls = app.config['IGNORE_URLS']
+   ignore_check_login_urls = app.config['IGNORE_CHECK_LOGIN_URLS']
 
-    # 如果是静态文件就不要查询用户信息了
-    pattern = re.compile('%s' % "|".join(ignore_check_login_urls))
-    if pattern.match(path):
-        return
-
-    if '/api' in path:
-        return
+   # 设置忽略静态资源  ^/static
+   pattern = re.compile("%s" % "|".join(ignore_check_login_urls))
+   if pattern.match(path):
+      return
 
 
-    user_info = check_login()
-    g.current_user = None
-    if user_info:
-        g.current_user = user_info
+   user_info = check_login()
+   g.current_user = None
+   if user_info:
+      g.current_user = user_info
 
-    pattern = re.compile('%s' % "|".join(ignore_urls))
-    if pattern.match(path):
-        return
+   # 设置/user/login的忽略规则，要用到正则表达式 ^/user/login
+   pattern = re.compile("%s" % "|".join(ignore_urls))
+   if pattern.match(path):
+      return
 
-    if not user_info :
-        return redirect( UrlManager.buildUrl( "/user/login" ) )
+   if not user_info:
+      return redirect(UrlManager.buildUrl("/user/login"))
+   return
 
-    return
 
-
-'''
-判断用户是否已经登录
-'''
+# 判断用户是否登录,返回用户信息user_info
 def check_login():
-    cookies = request.cookies
-    auth_cookie = cookies[app.config['AUTH_COOKIE_NAME']] if app.config['AUTH_COOKIE_NAME'] in cookies else None
+   cookies = request.cookies
+   auth_cookie = cookies[app.config['AUTH_COOKIE_NAME']] if app.config['AUTH_COOKIE_NAME'] in cookies else None
+   
+   if auth_cookie is None:
+      return False
+   # 取出uid
+   auth_info = auth_cookie.split("@")
+   if len(auth_info) != 2:
+      return False
+   
+   try:
+      user_info = User.query.filter_by(uid=auth_info[1]).first()
+   except Exception:
+      return False
 
-    if auth_cookie is None:
-        return False
+   if auth_info[0] != UserService.generateAuthCode(user_info):
+      return False
+   if user_info.status != 1:
+      return False
 
-    auth_info = auth_cookie.split("@")
-    if len(auth_info) != 2:
-        return False
-
-    try:
-        user_info = User.query.filter_by(uid=auth_info[1]).first()
-    except Exception:
-        return False
-
-    if user_info is None:
-        return False
-
-    if auth_info[0] != UserService.generateAuthCode( user_info ):
-        return False
-
-    if user_info.status != 1:
-        return False
-
-    return user_info
+   return user_info
